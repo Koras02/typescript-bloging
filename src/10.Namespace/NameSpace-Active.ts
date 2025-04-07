a9ab13ccf0e10e11e25011b8204714451927eb6c
Koras02 <wjdgh0727@gmail.com>
2025-04-07 09:54:55 +0900

    18.namespace

---
+++ src/10.Namespace/NameSpace-Active.ts
@@ -0,0 +1,11 @@
+namespace MathUtils {
+  export function add(a: number, b: number): number {
+    return a + b;
+  }
+
+  export function subtract(a: number, b: number): number {
+    return a - b;
+  }
+}
+
+console.log(MathUtils.add(5, 3)); // 8
---
+++ src/10.Namespace/tempCodeRunnerFile.ts
@@ -0,0 +1,5 @@
+import { MyApp } from './file1';
+import { MyApp as MyApp2 } from './file2';
+
+MyApp.functionA();
+MyApp2.functionB(); // Function B
