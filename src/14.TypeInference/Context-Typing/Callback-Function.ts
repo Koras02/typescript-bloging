9271483a43038fb0893a348ccc699c664dc35298
Koras02 <wjdgh0727@gmail.com>
2025-08-11 09:23:36 +0900

    22.TypeInference

---
+++ src/14.TypeInference/Context-Typing/Callback-Function.ts
@@ -0,0 +1,6 @@
+const nums = [1, 2, 3];
+
+nums.forEach((n) => {
+  // n: number 로 추론
+  console.log(n.toFixed);
+});
---
+++ src/14.TypeInference/Context-Typing/Event-Handler.ts
@@ -0,0 +1,4 @@
+window.addEventListener('click', (e) => {
+  // e: MouseEvent로 추론
+  console.log(e.clientX);
+});
